7b53d2fe6595a5f43b2d94222eb593814f5a4c6f
Hervé Saint-Amand <git@saintamh.org>
2019-03-10 11:23:51 +0000

    Better member selection

--- flott/flott.py
+++ flott/flott.py
@@ -9,6 +9,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # standards
 from glob import glob
+import logging
 from os.path import basename, join as path_join, splitext
 
 # flott
@@ -25,18 +26,19 @@ class Flott(object):
         self.namespace = dict(namespace)
         self.namespace[root_class.__name__] = root_class
 
-    def _load_members_from_file(self, file_path):
+    def _load_members_from_file(self, file_path, kwargs):
         file_locals = dict(self.namespace)
         try:
             execfile(file_path, file_locals, file_locals)
-        except Exception:
-            raise Exception("Failed to load '%s'" % file_path)
-        for key, value in file_locals.items():
-            if key not in self.namespace \
-                    and isinstance(value, type) \
-                    and issubclass(value, self.root_class):
-                member = value()
-                yield member
+        except Exception as error:
+            logging.error("Failed to load '%s': %s" % (file_path, error))
+        else:
+            for key, value in file_locals.items():
+                if key not in self.namespace \
+                        and isinstance(value, type) \
+                        and issubclass(value, self.root_class):
+                    member = value(**kwargs)
+                    yield member
 
     def _list_source_file_paths(self, sort_key=None):
         if sort_key is None:
@@ -50,9 +52,9 @@ class Flott(object):
             reverse=reverse,
         )
 
-    def all_members(self, sort_key=None):
+    def all_members(self, kwargs={}, sort_key=None):
         for file_path in self._list_source_file_paths(sort_key):
-            for member in self._load_members_from_file(file_path):
+            for member in self._load_members_from_file(file_path, kwargs):
                 yield member
 
     def load_member_by_id(self, member_id):
@@ -67,24 +69,26 @@ class Flott(object):
             min(all_member_ids, key=similarity)
         ))
 
-    def members_from_command_line_arguments(self, arguments):
-        selected_members = []
+    def members_from_command_line_arguments(self, arguments, **kwargs):
         if len(arguments) == 0:
-            selected_members = list(self.all_members())
+            for member in self.all_members(kwargs):
+                yield member
         else:
             selected_member_ids = set(arguments)
             for member in self.all_members(
+                    kwargs,
                     sort_key=self._string_similarity(arguments[0]),
                     ):
                 if member.id in selected_member_ids:
                     selected_member_ids.remove(member.id)
-                    selected_members.append(member)
+                    yield member
             if len(selected_member_ids) > 0:
+                # On the one hand, we'd like to raise this before we've loaded all members, but otoh this has to be a generator,
+                # because each member could have expensive resources associated with it. So we settle for this.
                 raise MemberNotFound("Unknown member%s: %s" % (
                     '' if len(selected_member_ids) == 1 else 's',
                     ', '.join(sorted(selected_member_ids)),
                 ))
-        return selected_members
 
     @classmethod
     def _string_similarity(cls, reference_text):
